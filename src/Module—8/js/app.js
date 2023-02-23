b8528894fafebe58ba9928badd298214d3eb59b7
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-23 16:36:24 +0200

    v

--- src/Module—8/js/app.js
+++ src/Module—8/js/app.js
@@ -5,3 +5,5 @@ const Joi = require("joi");
  const passwordSchema = Joi.string().min(3).max(14).alphanum();
  console.log(passwordSchema.validate('q'));
  console.log(passwordSchema.validate('YuriyPilipenko'));
+
+ 
