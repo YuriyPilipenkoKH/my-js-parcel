a56d574aa44cf45c1b1ff52d844e47a67d244608
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-28 19:33:35 +0200

    success

--- src/Module—9/js/color-switcher.js
+++ src/Module—9/js/color-switcher.js
@@ -5,10 +5,43 @@ const refs = {
 };
 
 refs.start.addEventListener('click', () => {
-  
+    // setBodyColor(getRandomHexColor())
+    bodyColorSetter.start()
 })
+
+refs.stop.addEventListener('click', () => {
+    bodyColorSetter.stop()
+})
+
+const bodyColorSetter = {
+    intervalId: null,
+    inProcess: false,
+
+    start() {
+        if(this.inProcess) {
+            return
+        }
+        this.inProcess = true
+        this.intervalId = setInterval(() => {
+        setBodyColor(getRandomHexColor())
+        }, 1000);
+    },
+
+    stop() {
+        clearInterval(this.intervalId)
+        this.inProcess = false
+},
+
+}
+
+
+
+function setBodyColor(color) {
+    document.body.style.backgroundColor = color;
+}
 
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+//   console.log(getRandomHexColor());
